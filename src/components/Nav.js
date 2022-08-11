d002cc906a717e8146ebb5dee350c41871d3e80d
DOMAYA <c2hila@gmail.com>
2022-08-11 20:22:11 +0900

    Nav 스타일 오류 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import "./Nav.css"
 
-function Nav(props) {
+function Nav() {
     const [show, setShow] = useState(false);
 
     
     useEffect(() => {
-        window.addEventListener("scroll", ()=>{
-            if(window.scrollY > 50){
-                setShow(true);
-            }else{
-                setShow(false);
-            }
-        })
-        return () =>{
-            window.removeEventListener("scroll", () => {
-
-            })
+        window.addEventListener("scroll", () => {
+          console.log("window.scrollY", window.scrollY);
+          if (window.scrollY > 50) {
+            setShow(true);
+          } else {
+            setShow(false);
+          }
+        });
+    
+        return () => {
+          window.removeEventListener("scroll", () => {});
         };
-    })
+      }, []);
 
 
   return (
-    <nav className={`nav ${show && 'nav__black'}`}>
+    <nav className={`nav ${show && "nav__black"} `}>
         <img 
                 alt = 'Netflix logo'
                 src= "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
